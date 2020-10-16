d3d2dff9a1710cf1f3abd4ca7dfe87e4b1ba7f18
suksien <hahasuksien@yahoo.com.my>
2020-10-16 16:17:19 +0800

    plotting carbon ion fractions

--- cloudy_runs/read_cloudy.py
+++ cloudy_runs/read_cloudy.py
@@ -1,10 +1,13 @@
 # 10/13/2020
-# modified from Koki (https://github.com/enigma-igm/xcorr_QSOfield/blob/master/cloudy/plot_cloudy.py)
+
 
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
-def read_cloudy(filename):
+def read_cloudy_koki(filename):
+    # modified from Koki (https://github.com/enigma-igm/xcorr_QSOfield/blob/master/cloudy/plot_cloudy.py)
+
     """
         read CLOUDY outputs (*.avr and *.ovr) and make a lookup table
         of various chemical elements as function of density and temperature
@@ -36,14 +39,38 @@ def x_civ_lookup(lookup):
     temp = lookup['Te']
     hden = lookup['hden']
     Z = lookup['METALS= %']
-    x_civ = lookup['IONI CARB 4 1']
+    civ_frac = lookup['IONI CARB 4 1']
 
-    lookup_new = pd.concat([x_CIV, hden, temp, Z], axis=1)
+    # use log10 values instead...?! Check skewers values
+    lookup_new = pd.concat([civ_frac, np.log10(hden), np.log10(temp), Z], axis=1)
 
     lookup_new.columns.values[0] = 'CIV_fraction'
     lookup_new.columns.values[-1] = 'Z'
 
+    lookup_arr = []
     return lookup_new
 
+def arr_lookup(lookup_new, Z, plot=False):
+    ind = np.where(lookup_new['Z'] == Z)[0]
+    hden = np.array(lookup_new['hden'][ind])
+    te = np.array(lookup_new['Te'][ind])
+    civ = np.array(lookup_new['CIV_fraction'][ind])
 
+    print(np.shape(hden), np.shape(te), np.shape(civ))
+
+    new_hden = np.reshape(hden, (61, 31))
+    new_te = np.reshape(te, (61, 31))
+    new_civ = np.reshape(civ, (61, 31))
+    print(np.min(new_civ), np.max(new_civ))
+
+    if plot:
+        plt.imshow(new_civ, vmin=0, vmax=0.6, extent=[3, 6, -6, -1])
+        plt.colorbar()
+        plt.xticks(np.arange(3, 6.5, 0.5))
+        plt.yticks(np.arange(-6, -0.5, 0.5))
+        plt.xlabel('log10(T)', fontsize=15)
+        plt.ylabel('log10(hden)', fontsize=15)
+        plt.show()
+
+    return new_hden, new_te, new_civ
 
