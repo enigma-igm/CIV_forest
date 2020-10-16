aeb2c85963cff727d906c6d2722f31fb117a102c
suksien <hahasuksien@yahoo.com.my>
2020-10-16 16:19:16 +0800

    plotting carbon ion fractions

--- cloudy_runs/read_cloudy.py
+++ cloudy_runs/read_cloudy.py
@@ -1,6 +1,3 @@
-# 10/13/2020
-
-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
@@ -33,38 +30,64 @@ def read_cloudy_koki(filename):
 
     return lookup
 
-def x_civ_lookup(lookup):
+def create_metal_lookup(filename, metal_name='CARB'):
 
-    # as a function of temperature, HI volume density, and metallicity
+    # metal_name options: 'CARB', 'OXYG', 'SILI'
+
+    ##### this block similar to read_cloudy_koki #####
+    print('reading ... : ', filename)
+    avr = pd.read_csv(filename + '.avr', sep='\t')  # load average ionization fraction of various speices
+    avr = avr.drop(avr.columns[0], axis=1)  # drop the first column as it is nan
+    ovr = pd.read_csv(filename + '.ovr',
+                      sep='\t')  # load overview file, which inlcude hydrogen density (hden [1/cm3]) and temperature (Te [K])
+    grd = pd.read_csv(filename + '.grd', sep='\t')  # grid file containing the values of the grid points
+    lookup = pd.concat([ovr, avr, grd], axis=1,
+                       sort=False)  # combine .ovr and .avr outputs into a summary CLOUDY lookup table
+
+    # remove leading and trailing white spaces in the column names
+    for i, col in enumerate(lookup):
+        lookup.columns.values[i] = col.strip()
+
     temp = lookup['Te']
     hden = lookup['hden']
     Z = lookup['METALS= %']
-    civ_frac = lookup['IONI CARB 4 1']
 
-    # use log10 values instead...?! Check skewers values
-    lookup_new = pd.concat([civ_frac, np.log10(hden), np.log10(temp), Z], axis=1)
+    # TODO: below currently only works for metals with ions up to 6
+    met1_frac = lookup['%s' % ('IONI ' + metal_name + ' 1 1')]
+    met2_frac = lookup['%s' % ('IONI ' + metal_name + ' 2 1')]
+    met3_frac = lookup['%s' % ('IONI ' + metal_name + ' 3 1')]
+    met4_frac = lookup['%s' % ('IONI ' + metal_name + ' 4 1')]
+    met5_frac = lookup['%s' % ('IONI ' + metal_name + ' 5 1')]
+    met6_frac = lookup['%s' % ('IONI ' + metal_name + ' 6 1')]
 
-    lookup_new.columns.values[0] = 'CIV_fraction'
-    lookup_new.columns.values[-1] = 'Z'
+    # use log10 values instead...?! Should maybe be consistent with skewers values
+    lookup_new = pd.concat([np.log10(hden), np.log10(temp), Z, \
+                            met1_frac, met2_frac, met3_frac, met4_frac, met5_frac, met6_frac], axis=1)
 
-    lookup_arr = []
+    lookup_new.columns.values[2] = 'Z'
+    lookup_new.columns.values[3] = metal_name + '1'
+    lookup_new.columns.values[4] = metal_name + '2'
+    lookup_new.columns.values[5] = metal_name + '3'
+    lookup_new.columns.values[6] = metal_name + '4'
+    lookup_new.columns.values[7] = metal_name + '5'
+    lookup_new.columns.values[8] = metal_name + '6'
+
     return lookup_new
 
-def arr_lookup(lookup_new, Z, plot=False):
+def arr_lookup(lookup_new, Z, metal_ion='CARB4', plot=False):
+
     ind = np.where(lookup_new['Z'] == Z)[0]
     hden = np.array(lookup_new['hden'][ind])
     te = np.array(lookup_new['Te'][ind])
-    civ = np.array(lookup_new['CIV_fraction'][ind])
+    ion = np.array(lookup_new[metal_ion][ind])
 
-    print(np.shape(hden), np.shape(te), np.shape(civ))
-
-    new_hden = np.reshape(hden, (61, 31))
+    new_hden = np.reshape(hden, (61, 31)) # hden grids x temperature grids
     new_te = np.reshape(te, (61, 31))
-    new_civ = np.reshape(civ, (61, 31))
-    print(np.min(new_civ), np.max(new_civ))
+    new_ion = np.reshape(ion, (61, 31))
 
     if plot:
-        plt.imshow(new_civ, vmin=0, vmax=0.6, extent=[3, 6, -6, -1])
+        plt.title(metal_ion, fontsize=18)
+        plt.imshow(new_ion, extent=[3, 6, -6, -1])
         plt.colorbar()
         plt.xticks(np.arange(3, 6.5, 0.5))
         plt.yticks(np.arange(-6, -0.5, 0.5))
@@ -72,5 +95,5 @@ def arr_lookup(lookup_new, Z, plot=False):
         plt.ylabel('log10(hden)', fontsize=15)
         plt.show()
 
-    return new_hden, new_te, new_civ
+    return new_ion
 
