63af93142e669c1baf0a38ef18e2837c4bd204d0
suksien <hahasuksien@yahoo.com.my>
2020-10-19 14:25:29 +0800

    updating plotting routines

--- cloudy_runs/plot_ion_diff.py
+++ cloudy_runs/plot_ion_diff.py
@@ -2,27 +2,33 @@ import numpy as np
 from matplotlib import pyplot as plt
 import read_cloudy as rc
 
-metal_ion_ls = ['CARB1', 'CARB2', 'CARB3', 'CARB4', 'CARB5', 'CARB5']
-out_filename = 'carbon_ion_diff.pdf'
+metal_ion_ls = ['IONI CARB 1 1', 'IONI CARB 2 1', 'IONI CARB 3 1', 'IONI CARB 4 1', 'IONI CARB 5 1', 'IONI CARB 6 1']
+#metal_ion_ls = ['IONI SILI 1 1', 'IONI SILI 2 1', 'IONI SILI 3 1', 'IONI SILI 4 1', 'IONI SILI 5 1', 'IONI SILI 6 1']
+out_filename = 'carbon_ion_reldiff.pdf'
 
-met_lookup = rc.create_metal_lookup('output/cloudy_grid')
+met_lookup = rc.read_cloudy_koki('output/cloudy_grid')
 
-plt.figure(figsize=(12,10))
+plt.figure(figsize=(10,8))
 for i in range(len(metal_ion_ls)):
-    ion1 = rc.arr_lookup(met_lookup, -3.5, metal_ion=metal_ion_ls[i])
-    ion2 = rc.arr_lookup(met_lookup, -1.5, metal_ion=metal_ion_ls[i])
-    diff = ion1 - ion2
+    ion1, nh_grid, temp_grid = rc.get_ion_frac(met_lookup, metal_ion_ls[i], -3.5)
+    ion1 = np.reshape(ion1, (61, 31))
+    ion2, nh_grid, temp_grid = rc.get_ion_frac(met_lookup, metal_ion_ls[i], -1.5)
+    ion2 = np.reshape(ion2, (61, 31))
 
-    plt.subplot(3,3,i+1)
-    plt.title(metal_ion_ls[i], fontsize=15)
+    diff = (ion1 - ion2)/ion1
+
+    plt.subplot(2,3,i+1)
+    plt.title(metal_ion_ls[i], fontsize=12)
     plt.imshow(diff, extent=[3, 6, -6, -1]) # 3 to 6 are the grid limits for log10(T)
                                                            # -6 to -1 are the grid limits for log10(hden)
     plt.colorbar()
     plt.xticks(np.arange(3, 6.5, 0.5))
     plt.yticks(np.arange(-6, -0.5, 0.5))
-    plt.xlabel('log10(T)', fontsize=13)
-    plt.ylabel('log10(hden)', fontsize=13)
+    plt.xlabel('log10(T)', fontsize=12)
+    plt.ylabel('log10(hden)', fontsize=12)
 
+plt.suptitle('Relative difference', fontsize=13)
 plt.tight_layout()
 plt.savefig(out_filename)
 plt.clf()
+#plt.show()
---
+++ cloudy_runs/plot_ion_frac2d.py
@@ -0,0 +1,30 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import read_cloudy as rc
+
+Z = -3.5
+metal_ion_ls = ['IONI CARB 1 1', 'IONI CARB 2 1', 'IONI CARB 3 1', 'IONI CARB 4 1', 'IONI CARB 5 1', 'IONI CARB 6 1']
+#metal_ion_ls = ['IONI SILI 1 1', 'IONI SILI 2 1', 'IONI SILI 3 1', 'IONI SILI 4 1', 'IONI SILI 5 1', 'IONI SILI 6 1']
+out_filename = 'carbon_ion_Z_-3.5.pdf'
+
+met_lookup = rc.read_cloudy_koki('output/cloudy_grid')
+
+plt.figure(figsize=(10,8))
+for i in range(len(metal_ion_ls)):
+    selected_ion_frac, nh_grid, temp_grid = rc.get_ion_frac(met_lookup, metal_ion_ls[i], Z)
+    selected_ion_frac = np.reshape(selected_ion_frac, (61, 31))
+
+    plt.subplot(2,3,i+1)
+    plt.title(metal_ion_ls[i], fontsize=15)
+    plt.imshow(selected_ion_frac, vmin=0, vmax=1, extent=[3, 6, -6, -1]) # 3 to 6 are the grid limits for log10(T)
+                                                                         # -6 to -1 are the grid limits for log10(hden)
+    plt.colorbar()
+    plt.xticks(np.arange(3, 6.5, 0.5))
+    plt.yticks(np.arange(-6, -0.5, 0.5))
+    plt.xlabel('log10(T)', fontsize=13)
+    plt.ylabel('log10(hden)', fontsize=13)
+
+plt.tight_layout()
+#plt.show()
+plt.savefig(out_filename)
+plt.clf()
