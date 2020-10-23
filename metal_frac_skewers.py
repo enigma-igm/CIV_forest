bf441509d9a68867064c4c4fed94b29a1c314d5a
suksien <hahasuksien@yahoo.com.my>
2020-10-23 15:54:25 +0800

    make general metal fraction skewers

--- metal_frac_skewers.py
+++ metal_frac_skewers.py
@@ -8,7 +8,6 @@ data_path = '/Users/suksientie/research/CIV_forest/sim_data/'
 tau_params = Table.read(data_path + 'rand_skewers_z45_ovt_tau.fits', hdu=1)
 tau_skewers = Table.read(data_path + 'rand_skewers_z45_ovt_tau.fits', hdu=2)
 """
-# LOOKUP_TABLE = cloudy_utils.read_cloudy_koki('')
 
 def make_xciv_skewer(params, skewers, cloudy_lookup, outfile):
     # modeled after enigma.tpe.utils.make_tau_skewers
@@ -23,6 +22,31 @@ def make_xciv_skewer(params, skewers, cloudy_lookup, outfile):
         xciv_out = cloudy_utils.get_ion_frac(cloudy_lookup, 'IONI CARB 4 1', -3.5, nh_skewers[i], temp_skewers[i])
         xciv_arr.append(xciv_out)
     skewers['X_CIV'] = xciv_arr
+
+    if outfile is not None:
+        hdu_param = fits.BinTableHDU(params.as_array())
+        hdu_table = fits.BinTableHDU(skewers.as_array())
+        hdulist = fits.HDUList()
+        hdulist.append(hdu_param)
+        hdulist.append(hdu_table)
+        hdulist.writeto(outfile, overwrite=True)
+        return True
+    else:
+        return params.as_array(), skewers.as_array()
+
+def make_xmetal_skewer(params, skewers, cloudy_lookup, cloudy_lookup_str, out_str, outfile):
+    # modeled after enigma.tpe.utils.make_tau_skewers
+
+    nh_bar = params['nH_bar']
+    nh_skewers = np.log10(nh_bar*skewers['ODEN']) # since the interpolation takes log10 units
+    temp_skewers = np.log10(skewers['T']) # since the interpolation takes log10 units
+
+    Nskew = len(skewers)
+    xmetal_arr = []
+    for i in range(Nskew): # ~1 min to process 10,000 skewers
+        xmetal_out = cloudy_utils.get_ion_frac(cloudy_lookup, cloudy_lookup_str, -3.5, nh_skewers[i], temp_skewers[i])
+        xmetal_arr.append(xmetal_out)
+    skewers[out_str] = xmetal_arr
 
     if outfile is not None:
         hdu_param = fits.BinTableHDU(params.as_array())
