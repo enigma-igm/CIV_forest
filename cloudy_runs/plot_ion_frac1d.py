265ea3eeb976d36dd23884330d338770e0c04293
suksien <hahasuksien@yahoo.com.my>
2020-11-02 15:19:16 +0800

    minor changes

--- cloudy_runs/plot_ion_frac1d.py
+++ cloudy_runs/plot_ion_frac1d.py
@@ -1,7 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import cloudy_utils as cu
-import pandas as pd
 
 # from skewers param file:
 # EOS-logT0 = 3.9574533249435015
@@ -11,16 +10,16 @@ Z = -3.5
 nh_bar_log10 = -4.5 # mean density
 temp_log10 = 4.0    # temperature at mean density
 
-metal_ion_ls = ['IONI CARB 2 1', 'IONI CARB 3 1', 'IONI CARB 4 1', 'IONI CARB 5 1', 'IONI CARB 6 1', 'IONI CARB 7 1']
+metal_ion_ls = ['IONI CARB 1 1', 'IONI CARB 2 1', 'IONI CARB 3 1', 'IONI CARB 4 1', 'IONI CARB 5 1', 'IONI CARB 6 1', 'IONI CARB 7 1']
 metal_ion_ls = ['IONI SILI 2 1', 'IONI SILI 3 1', 'IONI SILI 4 1', 'IONI SILI 5 1', 'IONI SILI 6 1', 'IONI SILI 7 1']
-#metal_ion_ls = ['IONI NITR 2 1', 'IONI NITR 3 1', 'IONI NITR 4 1', 'IONI NITR 5 1', 'IONI NITR 6 1', 'IONI NITR 7 1']
+metal_ion_ls = ['IONI NITR 2 1', 'IONI NITR 3 1', 'IONI NITR 4 1', 'IONI NITR 5 1', 'IONI NITR 6 1', 'IONI NITR 7 1']
 #metal_ion_ls = ['IONI OXYG 2 1', 'IONI OXYG 3 1', 'IONI OXYG 4 1', 'IONI OXYG 5 1', 'IONI OXYG 6 1', 'IONI OXYG 7 1']
 
-out_filename = 'silicon_ion_fraction.pdf'
+out_filename = 'plots/nitro_frac1d.pdf'
 
 met_lookup = cu.read_cloudy_koki('output/cloudy_grid_more')
-#total_frac_fixed_hden = np.zeros(31)
-#total_frac_fixed_temp = np.zeros(61)
+#total_frac_fixed_hden = np.zeros(51)
+#total_frac_fixed_temp = np.zeros(71)
 
 #plt.figure(figsize=(10,9))
 plt.figure(figsize=(10,5))
@@ -59,6 +58,6 @@ plt.legend()
 #plt.ylabel('Sum(ion fraction)', fontsize=13)
 
 plt.tight_layout()
-plt.show()
-#plt.savefig(out_filename)
-#plt.clf()
+#plt.show()
+plt.savefig(out_filename)
+plt.clf()
