75af0f4f48ce6d9ba4f5b1e4e0f6b66ac327c284
suksien <hahasuksien@yahoo.com.my>
2020-11-16 19:37:26 +0800

    shell script

---
+++ compute_model_grid_civ.sh
@@ -0,0 +1,3 @@
+# 20 min to finish
+python ~/Codes/enigma/enigma/reion_forest/compute_model_grid_civ.py --nproc 4 --fwhm 10 --samp 3 --SNR 20 --nqsos 10 --delta_z 0.2 --vmin 20 --vmax 2000 --dv 5 --ncovar 1000000 --nmock 500 --seed 1199
+
