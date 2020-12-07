45251ceab924704480b2db757f5618dc9aef4cf8
suksien <hahasuksien@yahoo.com.my>
2020-12-07 16:45:32 +0800

    trying to reproduce lit. plot

--- civ_cgm.py
+++ civ_cgm.py
@@ -3,6 +3,33 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from astropy.cosmology import Planck15
 
+def civ_dndNdz(n_star, alpha, N_star, N):
+
+    # Schechter function for CIV column density (N) distribution.
+    # note: small n refers to number
+
+    dn_dNdz = (n_star / N_star) * np.power(N / N_star, alpha) * np.exp(-N/ N_star)
+    return dn_dNdz
+
+def civ_dndz(n_star, alpha, N_star, N_min, N_max):
+    """
+    Compute Schechter integral over [N_min, N_max] interval using the incomplete gamma functions.
+    """
+
+    z = alpha + 1 # slope
+    upper = N_max / N_star
+    lower = N_min / N_star
+    # \Gamma(z, l, u) = \int_lower^upper x^(z-1) exp(-x) dx, where x = W/W_star
+    if isinstance(N_max,float):
+        I = float(mpmath.gammainc(z, lower, upper))
+    elif isinstance(N_max,np.ndarray):
+        I = np.zeros_like(N_max)
+        for indx in range(N_max.size):
+            I[indx] = float(mpmath.gammainc(z, lower, upper[indx]))
+
+    dn_dz = n_star * I
+    return dn_dz
+
 ### testing functions for CIV distribution functions from literatures.
 ### double-check before using
 def civ_dndzdW(W, z, type, k=None, alpha=None):
@@ -39,7 +66,7 @@ def civ_dndNdX(B, alpha, N_CIV):
     # alpha = 1.71 or 1.8 from D'Odorico et al. (2010)
     # alpha = 1.75 for D'Odorico et al. (2013), Figure 18, 4.35 < z < 5.3
     # what are obs values for B?
-    #    - log(B) = 9.5, by eye based on Figure 18 (i.e. for logN = 12.4 and logf = -12.2) of D'Odorico et al (2013)
+    #    - log(B) = 10.3, by eye based on Figure 18 (i.e. for logN = 12.4 and logf = -12.2) of D'Odorico et al (2013)
 
     dn_dNdX = B*N_CIV**(-alpha) # power law form for column density distribution function (CDDF)
     return dn_dNdX
@@ -59,7 +86,7 @@ def civ_dNdz(k, alpha, z, W_min, W_max, nW):
 
     return dN_dX, dN_dz
 
-def plot_cooksey2013_dndz():
+def cooksey2013_dndz():
     # Table 4 (CIV results summary)
     z_median = [1.96, 1.56, 1.66, 1.74, 1.82, 1.91, 2.02, 2.15, 2.36, 2.72, 3.26]
     z_min = [1.47, 1.47, 1.61, 1.70, 1.78, 1.87, 1.96, 2.08, 2.24, 2.51, 2.97]
@@ -82,4 +109,31 @@ def plot_cooksey2013_dndz():
     plt.xlabel('z', fontsize=13)
     plt.ylabel('dn/dz', fontsize=13)
     plt.tight_layout()
+    plt.show()
+
+# option 1: by-eye estimate the data points for f(N), then convert data points to dX --> dz
+# option 2: get dn/dN and provided you have dX (or dz), then just divide to get dn/dN/dZ
+# option 3: make use of omega_civ
+def dodorico2013_cddf():
+    # D'Odorico et al. (2013)
+    # data points for CDDF not provided, so estimating the points by eye from Figure 18
+    # for 4.35 < z < 5.3
+
+    logN_CIV = [12.82, 13.45, 13.64, 14.04]
+    logf = [-12.98, -12.97, -13.59, -14.39]
+
+    return logN_CIV, logf
+
+def reproduce_fig18():
+    B = 10**10.3
+    logN_CIV = np.arange(12.4, 15.2, 0.1)
+    alpha = 1.75
+
+    f = civ_dndNdX(B, alpha, 10**logN_CIV)
+    data_logN_CIV, data_logf = dodorico2013_cddf()
+
+    plt.plot(logN_CIV, np.log10(f), '--')
+    plt.plot(data_logN_CIV, data_logf, 'x')
+    plt.xlim([12.4, 15.2])
+    plt.ylim([-17.4, -11.2])
     plt.show()
