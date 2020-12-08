22ddb3e1cc3a19611836ee700e343574668fb366
suksien <hahasuksien@yahoo.com.my>
2020-12-08 15:41:03 +0800

    updates

--- civ_cgm.py
+++ civ_cgm.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from astropy.cosmology import Planck15
 
+# is this right?
 def civ_dndNdz(n_star, alpha, N_star, N):
 
     # Schechter function for CIV column density (N) distribution.
@@ -10,6 +11,23 @@ def civ_dndNdz(n_star, alpha, N_star, N):
 
     dn_dNdz = (n_star / N_star) * np.power(N / N_star, alpha) * np.exp(-N/ N_star)
     return dn_dNdz
+
+# dn_dNdX_sch = cgm.civ_dndNdz_test(1e-13, -0.8, 10**14, 10**logN_CIV, z=4.5) seems ok
+# when compared with dodorico2013_cddf()
+def civ_dndNdz_test(normalization, alpha, N_star, N, z=None):
+
+    # Schechter function for CIV column density (N) distribution.
+    # note: small n refers to number
+
+    dn_dNdz = normalization * np.power(N / N_star, alpha) * np.exp(-N/ N_star)
+    if z != None:
+        omega_m = Planck15.Om0
+        omega_lambda = 1 - omega_m
+        dz_dX = 1/(((1 + z) ** 2) * (omega_m * (1 + z) ** 3 + omega_lambda) ** (-0.5))
+        dn_dNdX = dn_dNdz * dz_dX
+        return dn_dNdX
+    else:
+        return dn_dNdz
 
 def civ_dndz(n_star, alpha, N_star, N_min, N_max):
     """
@@ -30,8 +48,7 @@ def civ_dndz(n_star, alpha, N_star, N_min, N_max):
     dn_dz = n_star * I
     return dn_dz
 
-### testing functions for CIV distribution functions from literatures.
-### double-check before using
+##### EW distribution function #####
 def civ_dndzdW(W, z, type, k=None, alpha=None):
     # dN, where N = number, not column density
 
@@ -62,15 +79,6 @@ def civ_dndzdW(W, z, type, k=None, alpha=None):
 
     return dn_dzdW, dn_dXdW
 
-def civ_dndNdX(B, alpha, N_CIV):
-    # alpha = 1.71 or 1.8 from D'Odorico et al. (2010)
-    # alpha = 1.75 for D'Odorico et al. (2013), Figure 18, 4.35 < z < 5.3
-    # what are obs values for B?
-    #    - log(B) = 10.3, by eye based on Figure 18 (i.e. for logN = 12.4 and logf = -12.2) of D'Odorico et al (2013)
-
-    dn_dNdX = B*N_CIV**(-alpha) # power law form for column density distribution function (CDDF)
-    return dn_dNdX
-
 # want to match to Simcoe et al. (2011) or Songaila (2001) dN/dz (which one?) by adjusting alpha and k
 def civ_dNdz(k, alpha, z, W_min, W_max, nW):
 
@@ -85,7 +93,30 @@ def civ_dNdz(k, alpha, z, W_min, W_max, nW):
     dN_dz = dN_dX * dX_dz
 
     return dN_dX, dN_dz
+##############################
 
+def civ_dndNdX(B, alpha, N_CIV):
+    # alpha = 1.71 or 1.8 from D'Odorico et al. (2010)
+    # alpha = 1.75 for D'Odorico et al. (2013), Figure 18, 4.35 < z < 5.3
+    # what are obs values for B?
+    #    - log(B) = 10.3, by eye based on Figure 18 (i.e. for logN = 12.4 and logf = -12.2) of D'Odorico et al (2013)
+
+    dn_dNdX = B*N_CIV**(-alpha) # power law form for column density distribution function (CDDF)
+    return dn_dNdX
+
+# to be verified if correct
+def civ_dndNdz(B, alpha, N_CIV, z):
+    dn_dNdX = B*N_CIV**(-alpha)
+
+    # converting to dNdzdW
+    omega_m = Planck15.Om0
+    omega_lambda = 1 - omega_m
+    dX_dz = ((1 + z) ** 2) * (omega_m * (1 + z) ** 3 + omega_lambda) ** (-0.5)  # correct for discrete dz??
+
+    dn_dNdz = dn_dNdX * dX_dz
+    return dn_dNdz
+
+##### data observations #####
 def cooksey2013_dndz():
     # Table 4 (CIV results summary)
     z_median = [1.96, 1.56, 1.66, 1.74, 1.82, 1.91, 2.02, 2.15, 2.36, 2.72, 3.26]
@@ -114,17 +145,24 @@ def cooksey2013_dndz():
 # option 1: by-eye estimate the data points for f(N), then convert data points to dX --> dz
 # option 2: get dn/dN and provided you have dX (or dz), then just divide to get dn/dN/dZ
 # option 3: make use of omega_civ
-def dodorico2013_cddf():
+def dodorico2013_cddf(z=None):
     # D'Odorico et al. (2013)
     # data points for CDDF not provided, so estimating the points by eye from Figure 18
     # for 4.35 < z < 5.3
 
     logN_CIV = [12.82, 13.45, 13.64, 14.04]
-    logf = [-12.98, -12.97, -13.59, -14.39]
+    logf = [-12.98, -12.97, -13.59, -14.39] # f = dn/dN/dX
 
+    """
+    if z != None:
+        omega_m = Planck15.Om0
+        omega_lambda = 1 - omega_m
+        dX_dz = ((1 + z) ** 2) * (omega_m * (1 + z) ** 3 + omega_lambda) ** (-0.5)
+        logf = np.array(logf) * dX_dz
+    """
     return logN_CIV, logf
 
-def reproduce_fig18():
+def reproduce_dodorico2013_fig18():
     B = 10**10.3
     logN_CIV = np.arange(12.4, 15.2, 0.1)
     alpha = 1.75
@@ -132,8 +170,11 @@ def reproduce_fig18():
     f = civ_dndNdX(B, alpha, 10**logN_CIV)
     data_logN_CIV, data_logf = dodorico2013_cddf()
 
-    plt.plot(logN_CIV, np.log10(f), '--')
+    plt.plot(logN_CIV, np.log10(f), '--', label=r'f(N) = B N$^{-\alpha}$')
     plt.plot(data_logN_CIV, data_logf, 'x')
+    plt.legend(fontsize=13)
+    plt.xlabel('log N(CIV)', fontsize=13)
+    plt.ylabel('log f', fontsize=13)
     plt.xlim([12.4, 15.2])
     plt.ylim([-17.4, -11.2])
     plt.show()
