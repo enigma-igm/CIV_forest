f9efd8959bbda8613cd94530706977718d3bfa35
suksien <hahasuksien@yahoo.com.my>
2020-12-16 19:53:45 +0800

    latest run config

--- compute_model_grid_civ.sh
+++ compute_model_grid_civ.sh
@@ -1,8 +1,10 @@
-# 20 min to finish
 now=`date`
 echo "Start: $now"
 
-python ~/Codes/enigma/enigma/reion_forest/compute_model_grid_civ.py --nproc 6 --fwhm 10 --samp 3 --SNR 40 --nqsos 20 --delta_z 0.2 --vmin 20 --vmax 2000 --dv 5 --ncovar 1000000 --nmock 500 --seed 1199 --nlogZ 9
+python ~/Codes/enigma/enigma/reion_forest/compute_model_grid_civ.py --nproc 4 --fwhm 10 --samp 3 --SNR 100 --nqsos 20 --delta_z 0.2 --vmin 20 --vmax 2000 --dv 5 --ncovar 1000000 --nmock 500 --seed 1190 --nlogZ 9
 
 now=`date`
 echo "Finish: $now"
+
+# 20 min run time for subset100 skewers
+# 2.5 hrs run time for the full 100,000 skewers with "--SNR 100 --nqsos 20 --delta_z 0.2 --vmin 20 --vmax 2000 --dv 5 --ncovar 1000000 --nmock 500 --seed 1190 --nlogZ 9" ... bottlenck is computing 2PCF (which needs to be done 2x for noisy and noiseless data)
