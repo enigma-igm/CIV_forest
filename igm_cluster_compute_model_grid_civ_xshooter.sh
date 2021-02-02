b032ddc818a14bb8b840455fb26843b5cd4b8952
suksien <hahasuksien@yahoo.com.my>
2021-02-02 13:36:23 +0800

    shell scripts to run compute_model on IGM cluster

---
+++ igm_cluster_compute_model_grid_civ_xshooter.sh
@@ -0,0 +1,7 @@
+now=`date`
+echo "Start: $now"
+
+python ~/codes/enigma/enigma/reion_forest/compute_model_grid_civ.py --nproc 31 --fwhm 30 --samp 3 --SNR 50 --nqsos 20 --delta_z 1.0 --vmin 20 --vmax 2000 --dv 10 --ncovar 1000000 --nmock 500 --seed 1259761 --logZmin -5.0 --logZmax -2.0 --nlogZ 31
+
+now=`date`
+echo "Finish: $now"
