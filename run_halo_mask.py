5ec8a1b9a7cc21b6c4205b6471aab73424884cee
suksien <hahasuksien@yahoo.com.my>
2021-02-18 12:21:36 +0800

    include model grid information

--- run_halo_mask.py
+++ run_halo_mask.py
@@ -9,14 +9,20 @@ outfile = 'nyx_sim_data/rand_skewers_z45_halomask_test.fits'
 #logM_grid, R_grid = halos_skewers.init_halo_grids()
 par, ske, halos = halos_skewers.init_all()
 
-zpix_mask00 = halos_skewers.calc_distance_all_skewers(par, ske, halos, 1.375, 10)
-zpix_mask01 = halos_skewers.calc_distance_all_skewers(par, ske, halos, 0.34375, 9)
+R = [1.375, 0.34375, 2.75]
+logM = [10, 9]
 
-table_mask = Table([zpix_mask00, zpix_mask01], names=('mask00', 'mask01'))
+zpix_mask00 = halos_skewers.calc_distance_all_skewers(par, ske, halos, 1.375, 10)
+zpix_mask11 = halos_skewers.calc_distance_all_skewers(par, ske, halos, 0.34375, 9)
+zpix_mask21 = halos_skewers.calc_distance_all_skewers(par, ske, halos, 2.75, 9)
+
+table_mask = Table([zpix_mask00, zpix_mask11, zpix_mask21], names=('mask00', 'mask11', 'mask21'))
+param = Table([[R], [logM]], names=('r_Mpc', 'logM'))
 
 print('Writing out to disk')
+hdu_param = fits.BinTableHDU(param.as_array())
 hdu_table = fits.BinTableHDU(table_mask.as_array())
-
 hdulist = fits.HDUList()
+hdulist.append(hdu_param)
 hdulist.append(hdu_table)
 hdulist.writeto(outfile, overwrite=True)
