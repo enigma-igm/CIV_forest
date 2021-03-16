c42dd6576f986aa3e787200789f01d81d7c81d85
suksien <hahasuksien@yahoo.com.my>
2021-03-16 14:22:11 -0700

    plotting

--- civ_cgm_pdf_enrichment.py
+++ civ_cgm_pdf_enrichment.py
@@ -7,9 +7,11 @@ from astropy import constants as const
 from astropy import units as u
 import civ_cgm
 import halos_skewers
+import time
 
 # to run on IGM machine where production files are
 taupath = '/mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/tau/'
+fvfm_igm_path = '/mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/fvfm_all.fits'
 
 # random seeds for drawing CGM absorbers
 seed = 102938
@@ -30,11 +32,14 @@ def init(R_want, logM_want, logZ=-3.5, metal_ion='C IV', fwhm=10, sampling=3.0):
 
     metal_dndz_func = civ_cgm.civ_dndz_sch
 
+    start = time.time()
     v_lores, (flux_tot_lores, flux_igm_lores, flux_cgm_lores), \
     v_hires, (flux_tot_hires, flux_igm_hires, flux_cgm_hires), \
     (oden, v_los, T, x_metal), cgm_tup = reion_utils.create_metal_forest(par, ske, logZ, fwhm, metal_ion, z=z, \
                                                                          sampling=sampling, cgm_dict=in_cgm_dict, \
                                                                          metal_dndz_func=metal_dndz_func, seed=seed)
+    end = time.time()
+    print("creating metal forest done in", (end-start)/60)
 
     return v_lores, flux_tot_lores, flux_igm_lores, flux_cgm_lores, \
            v_hires, flux_tot_hires, flux_igm_hires, flux_cgm_hires, cgm_tup, rand
@@ -97,14 +102,14 @@ def varying_fv(outfig, snr=50):
         #flux_noise_igm_lores = flux_igm_lores + noise
         #flux_noise_cgm_lores = flux_cgm_lores + noise
 
-        fv_want, fm_want = halos_skewers.get_fvfm(logM_want, Rval)
+        fv_want, fm_want = halos_skewers.get_fvfm(logM_want, Rval, fvfm_file=fvfm_igm_path)
         logZ_eff_want = halos_skewers.calc_igm_Zeff(fm_want, logZ_fid=-3.5)
 
         if iR == 0: # plot with noise
             plot_pdf_simple(flux_cgm_lores, label='CGM')
             plot_pdf_simple(noise, label='noise (SNR=%d)' % snr, noise=True)
 
-        plot_pdf_simple(flux_igm_lores, label=r'IGM (logZ$_{eff}$=%0.3f)' % logZ_eff_want)
+        plot_pdf_simple(flux_igm_lores, label=r'IGM (log$Z_{eff}=%0.3f$)' % logZ_eff_want)
 
-    plt.legend()
+    plt.legend(fontsize=10)
     plt.savefig(outfig)
