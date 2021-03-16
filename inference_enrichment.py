42f4004e5c2bdf5a3574209ffb80794cbd424d2d
suksien <hahasuksien@yahoo.com.my>
2021-03-15 17:29:23 -0700

    3d inference for enrichment model

---
+++ inference_enrichment.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import emcee
+import corner
+
+from scipy import optimize
+from IPython import embed
+from enigma.reion_forest.compute_model_grid import read_model_grid
+from enigma.reion_forest.utils import find_closest
+from enigma.reion_forest import inference
+import random
+
+######## Setting up #########
+seed = 125913 # random seed to pick the mock data set
+seed = None
+
+if seed == None:
+    seed = np.random.randint(0, 10000000)
+    print("Using random seed", seed)
+
+rand = np.random.RandomState(seed)
+random.seed(seed)
+
+def init(modelfile, logZ_guess, logM_guess, R_guess):
+
+    # Read in the model grid
+    params, xi_mock_array, xi_model_array, covar_array, icovar_array, lndet_array = read_model_grid(modelfile)
+
+    logZ_coarse = params['logZ'][0]
+    logM_coarse = params['logM'][0]
+    R_coarse = params['R_Mpc'][0]
+    R_coarse = np.round(R_coarse, 2) # need to force this to avoid floating point issue
+
+    vel_corr = params['vel_mid'].flatten()
+    vel_min = params['vmin_corr'][0]
+    vel_max = params['vmax_corr'][0]
+    nlogZ = params['nlogZ'][0]
+    nlogM = params['nlogM'][0]
+    nR = params['nR'][0]
+
+    # Pick the data that we will run with
+    nmock = xi_mock_array.shape[2]
+    imock = rand.choice(np.arange(nmock), size=1)
+
+    linearZprior = False
+
+    # find the closest model values to guesses
+    ilogZ = find_closest(logZ_coarse, logZ_guess)
+    ilogM =  find_closest(logM_coarse, logM_guess)
+    iR = find_closest(R_coarse, R_guess)
+
+    logZ_data = logZ_coarse[ilogZ]
+    logM_data = logM_coarse[ilogM]
+    R_data = R_coarse[iR]
+
+    xi_data = xi_mock_array[ilogM, iR, ilogZ, imock, :].flatten()
+    xi_mask = np.ones_like(xi_data, dtype=bool)  # in case you want to mask any xi value, otherwise all True
+
+    init_out = logZ_coarse, logM_coarse, R_coarse, xi_data, xi_mask, xi_model_array, lndet_array, icovar_array
+    return init_out
+
+import time
+def interp_likelihood(init_out, nlogZ_fine=501, nlogM_fine=501, nR_fine=501):
+
+    # unpack input
+    logZ_coarse, logM_coarse, R_coarse, xi_data, xi_mask, xi_model_array, lndet_array, icovar_array = init_out
+
+    # Interpolate the likelihood onto a fine grid to speed up the MCMC
+    nlogZ = logZ_coarse.size
+    logZ_fine_min = logZ_coarse.min()
+    logZ_fine_max = logZ_coarse.max()
+    dlogZ_fine = (logZ_fine_max - logZ_fine_min) / (nlogZ_fine - 1)
+    logZ_fine = logZ_fine_min + np.arange(nlogZ_fine) * dlogZ_fine
+
+    nlogM = logM_coarse.size
+    logM_fine_min = logM_coarse.min()
+    logM_fine_max = logM_coarse.max()
+    dlogM_fine = (logM_fine_max - logM_fine_min) / (nlogM_fine - 1)
+    logM_fine = logM_fine_min + np.arange(nlogM_fine) * dlogM_fine
+
+    nR = R_coarse.size
+    R_fine_min = R_coarse.min()
+    R_fine_max = R_coarse.max()
+    dR_fine = (R_fine_max - R_fine_min) / (nR_fine - 1)
+    R_fine = R_fine_min + np.arange(nR_fine) * dR_fine
+
+    print('dlogZ_fine', dlogZ_fine)
+    print('dlogM_fine', dlogM_fine)
+    print('dR', dR_fine)
+
+    # Loop over the coarse grid and evaluate the likelihood at each location
+    lnlike_coarse = np.zeros((nlogM, nR, nlogZ))
+
+    for ilogM, logM_val in enumerate(logM_coarse):
+        for iR, R_val in enumerate(R_coarse):
+            for ilogZ, logZ_val in enumerate(logZ_coarse):
+                lnlike_coarse[ilogM, iR, ilogZ] = inference.lnlike_calc(xi_data, xi_mask, xi_model_array[ilogM, iR, ilogZ, :],
+                                                            lndet_array[ilogM, iR, ilogZ],
+                                                            icovar_array[ilogM, iR, ilogZ, :, :])
+
+
+    lnlike_fine = inference.interp_lnlike_3d(logM_fine, R_fine, logZ_fine, logM_coarse, R_coarse, logZ_coarse, lnlike_coarse)
+
+    start = time.time()
+    xi_model_fine = inference.interp_model_3d(logM_fine, R_fine, logZ_fine, logM_coarse, R_coarse, logZ_coarse, xi_model_array)
+    end = time.time()
+    print((end-start)/60.)
+
+    return xi_model_fine
