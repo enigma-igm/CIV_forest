4b9447014fb0f56217851051ebdd0ae2fb542546
suksien <hahasuksien@yahoo.com.my>
2021-03-30 14:13:43 -0700

    updating everything

--- igm_cluster_compute_model_grid_civ_enrichment.sh
+++ igm_cluster_compute_model_grid_civ_enrichment.sh
@@ -9,3 +9,6 @@ python ~/codes/enigma/enigma/reion_forest/compute_model_grid_civ_new.py \
 
 now=`date`
 echo "Finish: $now"
+
+# ran in IGM on March 5, 2021:
+# nohup ./igm_cluster_compute_model_grid_civ_enrichment.sh > /mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/igm_cluster_compute_model_grid_civ_enrichment.log &
