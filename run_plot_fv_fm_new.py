fc44894763ea4115521562c3a38132c5fb9d1993
suksien <hahasuksien@yahoo.com.my>
2021-04-17 11:36:10 -0700

    paper plots

--- run_plot_fv_fm_new.py
+++ run_plot_fv_fm_new.py
@@ -3,25 +3,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 import halos_skewers
+import matplotlib.style as style
+style.use('tableau-colorblind10')
 
-##### to run on IGM cluster where all production files are #####
-maskpath = '/mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/xciv_mask/'
-outfig = '/mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/fvfm_all.png'
+##### requires the master table of fvfm (much faster)
+##### else needs to run on IGM where all production files are
+
+#maskpath = '/mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/xciv_mask/'
+#outfig = '/mnt/quasar/sstie/CIV_forest/Nyx_outputs/z45/enrichment_models/paper_plots/fvfm_all.pdf'
+
+fvfm_master = Table.read('nyx_sim_data/igm_cluster/enrichment_models/fvfm_all.fits')
+outfig = 'paper_plots/fvfm_all.pdf'
 
 # looking at just a subset of all models
 logM = np.array([8.50, 9.00, 9.50, 10.00, 10.50, 11.00])
-R = np.array([0.1, 0.50, 1.50, 3.00])
+R = np.array([0.1, 0.50, 1.00, 1.50, 2.00, 3.00])
 
 fm_vs_R = []
 fv_vs_R = []
 fm_vs_logM = []
 fv_vs_logM = []
 
+fvfm_master_logM = np.round(fvfm_master['logM'], 2)
+fvfm_master_R = np.round(fvfm_master['R_Mpc'], 2)
+
 # assemble things for plotting vs R
 for i_logM, logMval in enumerate(logM):
     temp_fm = []
     temp_fv = []
     for i_R, Rval in enumerate(R):
+        """
         mask_outfile = os.path.join(maskpath,
                                     'rand_skewers_z45_ovt_xciv_' + 'R_{:4.2f}'.format(Rval) + '_logM_{:4.2f}'.format(
                                         logMval) + '.fits')
@@ -33,6 +44,10 @@ for i_logM, logMval in enumerate(logM):
         temp_fv.append(fv)
         ske = None
         mask_arr = None
+        """
+        want_i = np.where((fvfm_master_logM == logMval) & (fvfm_master_R == Rval))[0][0]
+        temp_fm.append(fvfm_master['fm'][want_i])
+        temp_fv.append(fvfm_master['fv'][want_i])
 
     fm_vs_R.append(temp_fm)
     fv_vs_R.append(temp_fv)
@@ -42,6 +57,7 @@ for i_R, Rval in enumerate(R):
     temp_fm = []
     temp_fv = []
     for i_logM, logMval in enumerate(logM):
+        """
         mask_outfile = os.path.join(maskpath, 'rand_skewers_z45_ovt_xciv_' + 'R_{:4.2f}'.format(Rval) + '_logM_{:4.2f}'.format(logMval) + '.fits')
         ske = Table.read(mask_outfile, hdu=2)
         mask_arr = ske['MASK'].astype(bool)
@@ -51,33 +67,48 @@ for i_R, Rval in enumerate(R):
         temp_fv.append(fv)
         ske = None
         mask_arr = None
+        """
+        want_i = np.where((fvfm_master_logM == logMval) & (fvfm_master_R == Rval))[0][0]
+        temp_fm.append(fvfm_master['fm'][want_i])
+        temp_fv.append(fvfm_master['fv'][want_i])
 
     fm_vs_logM.append(temp_fm)
     fv_vs_logM.append(temp_fv)
 
 # now let's plot things
-plt.figure(figsize=(12,5))
-plt.subplot(121)
+plt.figure(figsize=(13,10.5))
+plt.subplot(222)
 for i_logM, logMval in enumerate(logM):
     plt.plot(R, fv_vs_R[i_logM], 'o-', label=r'log$_{10}M$ = %0.2f' % logMval)
-for i_logM, logMval in enumerate(logM):
-    plt.plot(R, fm_vs_R[i_logM], 'o--')
 plt.legend(fontsize=13)
 plt.xlabel('R (Mpc)', fontsize=18)
-plt.ylabel('Filling factor', fontsize=18)
 plt.gca().tick_params(axis="x", labelsize=13)
 plt.gca().tick_params(axis="y", labelsize=13)
 
-plt.subplot(122)
+plt.subplot(221)
 for i_R, Rval in enumerate(R):
     plt.plot(logM, fv_vs_logM[i_R], 'o-', label='R = %0.2f Mpc' % Rval)
-for i_R, Rval in enumerate(R):
-    plt.plot(logM, fm_vs_logM[i_R], 'o--')
 plt.legend(fontsize=13)
 plt.xlabel(r'log$_{10}M$', fontsize=18)
-plt.gca().axes.yaxis.set_visible(False)
+plt.ylabel(r'$f_v$', fontsize=18)
+plt.gca().tick_params(axis="x", labelsize=13)
+
+plt.subplot(224)
+for i_logM, logMval in enumerate(logM):
+    plt.plot(R, fm_vs_R[i_logM], 'o-', label=r'log$_{10}M$ = %0.2f' % logMval)
+#plt.legend(fontsize=13)
+plt.xlabel('R (Mpc)', fontsize=18)
+plt.gca().tick_params(axis="x", labelsize=13)
+plt.gca().tick_params(axis="y", labelsize=13)
+
+plt.subplot(223)
+for i_R, Rval in enumerate(R):
+    plt.plot(logM, fv_vs_logM[i_R], 'o-', label='R = %0.2f Mpc' % Rval)
+#plt.legend(fontsize=13)
+plt.xlabel(r'log$_{10}M$', fontsize=18)
+plt.ylabel(r'$f_m$', fontsize=18)
 plt.gca().tick_params(axis="x", labelsize=13)
 
 plt.tight_layout()
-plt.show()
-#plt.savefig(outfig)
+#plt.show()
+plt.savefig(outfig)
--- run_plot_zeff.py
+++ run_plot_zeff.py
@@ -2,18 +2,53 @@ from astropy.table import Table
 import numpy as np
 from matplotlib import pyplot as plt
 import halos_skewers
+import matplotlib.style as style
+style.use('tableau-colorblind10')
 
-fvfm_all = Table.read('nyx_sim_data/igm_cluster/fvfm_all.fits')
+outfig = 'paper_plots/logZeff.pdf'
+
+fvfm_master = Table.read('nyx_sim_data/igm_cluster/enrichment_models/fvfm_all.fits')
+fvfm_master_logM = np.round(fvfm_master['logM'], 2)
+fvfm_master_R = np.round(fvfm_master['R_Mpc'], 2)
+
 logZ_fid = -3.5
 plot_vs_R = False # if False, then plot vs logM (at fixed R)
 
 # looking at just a subset of all models
-logM, R = halos_skewers.init_halo_grids(8.5, 11.0, 0.50, 0.1, 3, 0.4)
-R = R[:-1] # the last element is outside model boundaries
+logM = np.array([8.50, 9.00, 9.50, 10.00, 10.50, 11.00])
+R = np.array([0.1, 0.50, 1.00, 1.50, 2.00, 3.00])
 
 # all models
-logM, R = halos_skewers.init_halo_grids(8.5, 11.0, 0.25, 0.1, 3, 0.2)
+#logM, R = halos_skewers.init_halo_grids(8.5, 11.0, 0.25, 0.1, 3, 0.2)
 
+plt.figure(figsize=(13.5,6.5))
+plt.subplot(121)
+for i_R, Rval in enumerate(R):
+    want_iR = np.where(fvfm_master_R == Rval)[0]
+    logZ_eff = halos_skewers.calc_igm_Zeff(fvfm_master['fm'][want_iR], logZ_fid=logZ_fid)
+    plt.plot(fvfm_master['logM'][want_iR], logZ_eff, 'o-', ms=3, label='R = %0.2f Mpc' % Rval)
+
+plt.xlabel('logM', fontsize=18)
+plt.ylabel(r'logZ$_{eff}$', fontsize=18)
+plt.legend(fontsize=13, ncol=2)
+plt.gca().tick_params(axis="x", labelsize=13)
+plt.gca().tick_params(axis="y", labelsize=13)
+
+plt.subplot(122)
+for i_logM, logMval in enumerate(logM):
+    want_ilogM = np.where(fvfm_master_logM == logMval)[0]
+    logZ_eff = halos_skewers.calc_igm_Zeff(fvfm_master['fm'][want_ilogM], logZ_fid=logZ_fid)
+    plt.plot(fvfm_master['R_Mpc'][want_ilogM], logZ_eff, 'o-', ms=3, label='logM = %0.2f' % logMval)
+
+plt.xlabel('R (Mpc)', fontsize=15)
+plt.legend(fontsize=12)
+plt.gca().tick_params(axis="x", labelsize=13)
+plt.gca().tick_params(axis="y", labelsize=13)
+
+plt.tight_layout()
+plt.savefig(outfig)
+
+"""
 plt.figure(figsize=(8,6))
 if plot_vs_R:
     for i_logM, logMval in enumerate(logM):
@@ -41,3 +76,4 @@ plt.ylabel(r'logZ$_{eff}$', fontsize=15)
 plt.title(r'logZ$_{fid}$ = %0.1f' % logZ_fid)
 plt.tight_layout()
 plt.show()
+"""
